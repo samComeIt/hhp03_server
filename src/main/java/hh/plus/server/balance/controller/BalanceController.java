83bd3dfc335d6c0f0fbe5c1c05a5e73e633a2523
김산해 <sanhae1996@gmail.com>
2024-07-09 23:57:30 +0900

    BalanceController Swagger 정보 추가

--- src/main/java/hh/plus/server/balance/controller/BalanceController.java
+++ src/main/java/hh/plus/server/balance/controller/BalanceController.java
@@ -4,10 +4,16 @@ import hh.plus.server.balance.controller.dto.BalanceDto;
 import hh.plus.server.balance.controller.dto.BalanceResponseDto;
 import hh.plus.server.balance.domain.entity.Balance;
 import hh.plus.server.balance.service.BalanceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.Table;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Balance API")
 @RequestMapping("/api/balance")
 @RequiredArgsConstructor
 public class BalanceController {
@@ -18,6 +24,11 @@ public class BalanceController {
      * @param balanceId
      * @return
      */
+    @Operation(summary = "Get balance by id", description = "Returns a balance as per the id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - The balance does not exist"),
+    })
     @GetMapping("/{balanceId}")
     public BalanceResponseDto getBalance(@PathVariable long balanceId){
         return balanceService.getBalanceByBalanceId(balanceId);
@@ -29,9 +40,13 @@ public class BalanceController {
      * @param amount
      * @return
      */
+    @Operation(summary = "Update balance by id and amount", description = "Returns updated balance")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated"),
+            @ApiResponse(responseCode = "404", description = "Not found - The balance does not exist")
+    })
     @PatchMapping("/{balanceId}")
     public BalanceResponseDto updateBalance(@PathVariable long balanceId, @RequestBody long amount){
         return balanceService.updateBalance(balanceId, amount);
-        //return BalanceResponseDto.response(balanceId, amount);
     }
 }
