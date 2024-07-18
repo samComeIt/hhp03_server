a4bf829ec768e0b1e46ebc6c11f01eca7af1e79d
김산해 <sanhae1996@gmail.com>
2024-07-18 21:03:34 +0900

    Balance CustomException 추가

--- src/main/java/hh/plus/server/balance/service/BalanceService.java
+++ src/main/java/hh/plus/server/balance/service/BalanceService.java
@@ -3,6 +3,7 @@ package hh.plus.server.balance.service;
 import hh.plus.server.balance.service.dto.BalanceResponseDto;
 import hh.plus.server.balance.domain.entity.Balance;
 import hh.plus.server.balance.domain.repository.BalanceRepository;
+import hh.plus.server.common.exception.CustomException;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,7 @@ public class BalanceService {
         log.info("updateBalance : {}, {}", balanceId, amount);
 
         Balance balance = balanceRepository.findById(balanceId)
-                .orElseThrow(() -> new IllegalArgumentException("Balance not found"));
+                .orElseThrow(() -> new CustomException("Balance not found"));
 
         balance.newBalance(amount);
         balanceRepository.save(balance);
@@ -35,7 +36,7 @@ public class BalanceService {
         log.info("getBalanceByBalanceId : {}", balanceId);
 
         Balance balance = balanceRepository.findById(balanceId)
-                .orElseThrow(() -> new IllegalArgumentException("Balance not found"));
+                .orElseThrow(() -> new CustomException("Balance not found"));
 
         return new BalanceResponseDto(balance.getBalanceId(), balance.getBalance());
     }
