eb681baa4e31b781ea5a9475fe7866b735e41872
김산해 <sanhae1996@gmail.com>
2024-07-21 13:54:09 +0900

    CartController 추가

--- src/main/java/hh/plus/server/cart/controller/CartController.java
+++ src/main/java/hh/plus/server/cart/controller/CartController.java
@@ -1,24 +1,24 @@
 package hh.plus.server.cart.controller;
 
+import hh.plus.server.cart.domain.entity.Cart02;
 import hh.plus.server.cart.service.CartService;
 import hh.plus.server.cart.service.dto.Cart02Dto;
+import hh.plus.server.cart.service.dto.CartCreateRequestDto;
 import hh.plus.server.cart.service.dto.CartDto;
 import hh.plus.server.order.domain.entity.Order;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@Tag(name = "Order API")
+@Tag(name = "Cart API")
 @RequestMapping("/api/cart")
 @RequiredArgsConstructor
 public class CartController {
@@ -33,5 +33,20 @@ public class CartController {
     public Cart02Dto getCart(@PathVariable long cartId){ return cartService.getCart02ById(cartId);}
 
 
+    @Operation(summary = "Create cart", description = "Returns a created cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created"),
+            @ApiResponse(responseCode = "404", description = "Not found - The cart does not exist"),
+    })
+    @PostMapping("/create")
+    public Cart02 createCart(@RequestBody CartCreateRequestDto cartCreateRequestDto){ return cartService.createCart(cartCreateRequestDto);}
+
+    @Operation(summary = "Delete cart", description = "Returns deleted cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "Not found - The cart does not exist"),
+    })
+    @DeleteMapping("/{cartId}")
+    public void deleteCart(@PathVariable long cartId){ cartService.deleteCart(cartId);}
 
 }
