5ab9f053ed32abced484deaf2dd358d705473519
김산해 <sanhae1996@gmail.com>
2024-07-26 00:37:44 +0900

    불필요한 소스 정리

--- src/test/java/hh/plus/server/order/OrderServiceTest.java
+++ src/test/java/hh/plus/server/order/OrderServiceTest.java
@@ -1,19 +1,11 @@
 package hh.plus.server.order;
 
-import hh.plus.server.order.domain.entity.Order;
-import hh.plus.server.order.domain.entity.OrderDetail;
 import hh.plus.server.order.service.OrderService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootTest
 public class OrderServiceTest {
@@ -25,18 +17,7 @@ public class OrderServiceTest {
     @DisplayName("주문 생성 성공 케이스")
     public void testCreateOrderSuccess()
     {
-        // given
-        List<OrderDetail> orderDetail = Arrays.asList(
-          new OrderDetail()
-        );
 
-        Order order = new Order(101L, "INCOMPLETE", orderDetail, LocalDateTime.now(), LocalDateTime.now());
-
-        // when
-        Order mockOrder = orderService.createOrder(order);
-
-        // then
-        assertNotNull(mockOrder);
     }
 
 }
