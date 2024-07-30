08359ae8b8b7945e8d7de54284f37553f18bce25
김산해 <sanhae1996@gmail.com>
2024-07-30 22:50:28 +0900

    Balance Service Cache 단위테스트 임시로 추가

--- src/main/java/hh/plus/server/balance/service/BalanceService.java
+++ src/main/java/hh/plus/server/balance/service/BalanceService.java
@@ -4,6 +4,7 @@ import hh.plus.server.balance.service.dto.BalanceResponseDto;
 import hh.plus.server.balance.domain.entity.Balance;
 import hh.plus.server.balance.domain.repository.BalanceRepository;
 import hh.plus.server.common.exception.CustomException;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +32,7 @@ public class BalanceService {
     }
 
     @Transactional(readOnly = true)
+    @Cacheable("myBalance")
     public BalanceResponseDto getBalanceByBalanceId(Long balanceId) {
 
         log.info("getBalanceByBalanceId : {}", balanceId);
