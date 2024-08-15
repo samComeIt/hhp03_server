2cc93677f6153538090a30509e70b44214af6f4e
김산해 <sanhae1996@gmail.com>
2024-08-15 17:00:10 +0900

    EventListener @TransactionalEventListener 수정 및 kafkaProducerService.sendMessage 추가

--- src/main/java/hh/plus/server/order/presentation/listener/OrderCreatedEventListener.java
+++ src/main/java/hh/plus/server/order/presentation/listener/OrderCreatedEventListener.java
@@ -4,18 +4,14 @@ import hh.plus.server.common.service.KafkaProducerService;
 import hh.plus.server.order.domain.listener.OrderCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class OrderEventListener {
+public class OrderCreatedEventListener {
     private KafkaProducerService kafkaProducerService;
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handleOrderCreatedEvent(OrderCreatedEvent event) {
